692149970cfa0319199db6d012a7a9ae31354842
urgobalt <lukecastellan165@gmail.com>
2021-11-08 17:47:49 +0100

    Project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+import Layout from '../layouts/Default'
+
+const Home: NextPage = () => {
+  return (
+    <Layout title="Hem">
+      <h1>Hello</h1>
+    </Layout>
+  )
+}
+
+export default Home
